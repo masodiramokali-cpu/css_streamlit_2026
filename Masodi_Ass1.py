fd1a473ff918960c1f318af016bbb499e1a80309
masodiramokali-cpu <masodiramokali@gmail.com>
2026-01-31 10:20:33 +0200

    Add files via upload

---
+++ Masodi_Ass1.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 30 07:48:14 2026
+
+@author: Masodi Ramokali
+"""
+
+import streamlit as st
+
+st.title("MCR Sneakers")
+
+category = st.selectbox("Select Category", ["Children","Adults"])
+
+gender = st.selectbox("Select Gender", ["Female","Male"])
+
+size = st.selectbox("Select Size", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+if category == "Children":
+    price = 600
+else: 
+    price = 1000
+    
+quantity = st.number_input("Quantity", min_value=1, value=1)
+
+total = price * quantity 
+
+st.write("### Order Details")
+st.write("Sneaker: The Swegg")
+st.write("Category:", category)
+st.write("Gender:", gender)
+st.write("Size:", size)
+st.write("Price per pair: R", price)
+st.write("Total price: R", total)
+
+if st.button("Buy Now"):
+    st.success("Purchase successful!")
